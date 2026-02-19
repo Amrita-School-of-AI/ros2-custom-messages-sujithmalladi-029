38423068ab61958573c903fdddc8f425e2f97dda
Sujith Malladi <sujithmalladi@gmail.com>
2026-02-19 23:10:16 +0530

    Add prebuilt ROS2 workspace for Classroom autograder

---
+++ build/ros2_custom_msgs/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/malladi-sujith-babu/ros2_ws/src/ros2-custom-messages-sujithmalladi-029/ros2_custom_msgs
+# Build directory: /home/malladi-sujith-babu/ros2_ws/src/ros2-custom-messages-sujithmalladi-029/build/ros2_custom_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("ros2_custom_msgs__py")
